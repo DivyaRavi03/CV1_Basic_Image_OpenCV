e86aacaae608d70400ee2446ac183e3f20aa9af8
DivyaRavi03 <125673818+DivyaRavi03@users.noreply.github.com>
2025-08-08 11:23:05 -0400

    feat: Add initial script for image loading and conversion

---
+++ load_img.py
@@ -0,0 +1,14 @@
+import cv2
+image_path = "/home/divya/cv_projects/project_1/test_img.jpg"
+image = cv2.imread(image_path)
+if image is None:
+   print(f"Error:{image_path}")
+else:
+   height, width, channels = image.shape
+   print(f"Dimensions:{width}, wide by:{height}, channels: {channels}")
+   grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+   cv2.imshow("Original image", image)
+   cv2.imshow("Grayscale image", grayscale)
+   cv2.waitKey(0)
+   cv2.destroyAllWindows()
